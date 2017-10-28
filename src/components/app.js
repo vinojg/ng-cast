e2b31e6a143dcea553ae8991a496468051f9ae18
Hack Reactor Student <student@hackreactor.com>
2017-10-27 19:57:20 -0700

    Working on getting data from API

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,11 @@
 angular.module('video-player')
   .component('app', {
     templateUrl: 'src/templates/app.html',
-    controller: function () {
+    controller: function (youTube) {
+      this.dataBlob;
+      // youTube.search(query, function() {
+        
+      // })
       this.videos = window.exampleVideoData;
       this.video = window.exampleVideoData[0];
       this.videoListEntryClick = (obj) => {
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,57 @@
 angular.module('video-player')
-.service('youTube', function(){
+  .service('youTube', function($http) {
   // TODO
-});
+  
+    $http({
+      method: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      params: {
+        maxResults: 5,
+        key: window.YOUTUBE_API_KEY,
+        part: 'snippet',
+        query: 'puppies',
+        type: 'video',
+        videoEmbeddable: true
+      }
+    }).then(function successCallback(response) {
+      dataBlob = response;
+      console.log('success', dataBlob);
+      console.log('success', response);
+      // this callback will be called asynchronously
+      // when the response is available
+    }, function errorCallback(response) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      console.error('backcats: Failed to send message', response);
+    });
+  });
+  
+  
+//   window.firstLoad = true;
+// var searchYouTube = ({max, key, query}, context) => {
+//   // TODO
+//   $.ajax({
+//     url: 'https://www.googleapis.com/youtube/v3/search',
+//     type: 'GET',
+//     params: {
+//       maxResults: max,
+//       key: key,
+//       part: 'snippet',
+//       q: query,
+//       type: 'video'
+//     },
+//     success: function (data) {
+//       var dataBlob = data;
+//       if (!window.firstLoad) {
+//         context.setState({videoCollection: dataBlob.items});
+//       }
+//       if (window.firstLoad) {
+//         ReactDOM.render(<App videoData={dataBlob.items}/>, document.getElementById('app'));
+//         window.firstLoad = false;
+//       }
+//     },
+//     error: function (data) {
+      
+//     }
+//   });
+// };
